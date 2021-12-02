79a74a0487128dd49827c2d2924b02128fe1f6f9
Hannah Bacon <hannahbacon@hotmail.co.uk>
2021-12-02 18:00:19 +0000

    Add get submarine position functionality. Add challenge data input

--- dive.py
+++ dive.py
@@ -1,3 +1,24 @@
 
 def get_submarine_position(filename):
-    return
+    x = 0
+    y = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            inputs = line.split(' ')
+            direction = inputs[0]
+            length = int(inputs[1])
+
+            if direction == "forward":
+                x += length
+            elif direction == "down":
+                y += length
+            elif direction == "up":
+                y -= length
+
+    return x, y
+
+
+if __name__ =="__main__":
+    x, y =get_submarine_position('dive_input.txt')
+    print(x, y)
+    print(x*y)
--- tests/test_dive.py
+++ tests/test_dive.py
@@ -2,4 +2,5 @@ from dive import get_submarine_position
 
 def test_get_submarine_position():
     result = get_submarine_position('tests/test_dive_input.txt')
-    assert result == 150
+    assert result[0] == 15
+    assert result[1] == 10
