0021cbea71fa0e625f03f9fc3c2c9d29f93895b4
Hannah Bacon <hannahbacon@hotmail.co.uk>
2021-12-02 16:08:05 +0000

    Add get depth changes function

--- depths.py
+++ depths.py
@@ -1,3 +1,19 @@
 
 def get_depth_changes(filename):
-    return
+    with open(filename, 'r') as f:
+        counter = 0
+        prev_depth = None
+        times_increased = 0
+        for line in f:
+            depth = int(line)
+            if counter ==0:
+                pass
+            else:
+                if prev_depth < depth:
+                    times_increased +=1
+            prev_depth = depth
+            counter +=1
+        return times_increased
+
+if __name__ =="__main__":
+    get_depth_changes('depths_input.txt')
--- tests/test_depth.py
+++ tests/test_depth.py
@@ -1,5 +1,5 @@
 from depths import get_depth_changes
 
 def test_get_depth_changes():
-    result = get_depth_changes('test_depths_input.txt')
+    result = get_depth_changes('tests/test_depths_input.txt')
     assert result == 7
